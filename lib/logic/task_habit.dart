3ca13142628bae5b675e6913f0fd45bdbda123ec
Raihan Athallah <rathallah03@gmail.com>
2026-05-02 11:17:04 +0700

    task_habit

--- lib/logic/task_habit.dart
+++ lib/logic/task_habit.dart
@@ -1,1 +1,43 @@
-// Tes
+/// Class Habit (simpan data kebiasaan)
+
+
+
+
+
+
+
+
+/// Class ToDo ( simpan data tugas)
+
+
+
+
+
+
+
+//-----------------
+/// Function Habit
+//-----------------
+
+
+
+
+
+
+
+//-----------------
+/// Function Todo
+//-----------------
+
+
+
+
+
+
+
+
+
+
+//-----------------
+/// Function Reward
+//-----------------
