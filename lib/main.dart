bc8609c2e9c51bdb7d10aeacd8881984404aeb2b
Raihan Athallah <rathallah03@gmail.com>
2026-05-03 11:53:33 +0700

    tes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'logic/taskhabit_logic.dart';
+import 'logic/taskhabit_logic.dart'; // task&habit manag
 
 void main() {
   runApp(const MyApp());
@@ -29,9 +29,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Habit Test'),
     );
   }
 }
@@ -86,33 +86,29 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          
+          Text("Jumlah air: $air"),
+          SizedBox(height: 20),
+
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                if (habitList.isNotEmpty) {
+                  checkHabit(0);
+                }
+              });
+            },
+
+            child: Text("Checklist Habit"),
             ),
+
           ],
         ),
-      ),
+      
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
